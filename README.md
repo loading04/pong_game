061d16ae5e858770283a9274b53da326db578dee
aziz <ahmed.aziz.smaoui@gmail.com>
2023-01-19 17:07:55 +0100

    Pong game -V0.1

---
+++ README.md
@@ -0,0 +1,22 @@
+
+
+## About The Project
+
+[game-screenshot]
+pong game multiplayer local
+
+### Built With
+[![python][python-pi]][python-url]
+
+## Getting Started
+
+You can just RUN main.py, or you can play the executable file /output/main.exe
+Right player (paddle) can play with Up and Down arrow
+Left player (paddle) can play with Z and S
+You can change the controls in main.py line 36
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[game-screenshot]: images/pong.png
+[python-pi]:https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white
+[python-url]:https://www.python.org/
+
---
+++ main.py
@@ -0,0 +1,67 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+# UI of the game
+ui = Turtle()
+ui.speed("fastest")
+ui.color("white")
+ui.penup()
+ui.goto(0, 300)
+ui.pendown()
+ui.pensize(15)
+ui.shapesize(stretch_wid=1, stretch_len=1)
+ui.setheading(270)
+for _ in range(15):
+    ui.pendown()
+    ui.forward(20)
+    ui.penup()
+    ui.forward(40)
+
+# Creating new paddles
+scoreboard = Scoreboard()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "z")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 279 or ball.ycor() < -279:
+        # need to bounce
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
